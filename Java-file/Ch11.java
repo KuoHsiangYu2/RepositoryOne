f7025add9522f485166f25c1d57eca7c74e09716
hsiangyukuo <bbb105013@nptu.edu.tw>
2020-02-23 22:08:15 +0800

    1. 重新修改變數名稱為有意義的名稱。 2. 替變數設定初始值。 3. 修改函式名稱符合規定。 4. 把 println() 修改成 printf 方便格式化輸出。 5. 額外加上 +1 的設定，讓人數從1開始計數。 6. 重新在共同的父類別定義相同的抽象函式。

--- Java-file/Ch11.java
+++ Java-file/Ch11.java
@@ -1,74 +1,79 @@
 //尚未完成
 package ch11_6;
 
+import static java.lang.System.out;
+
 import java.util.Scanner;
 
 abstract class Parent {
-    String name[] = new String[10];
-    int data[][] = new int[10][5];
-    String str1, str2;
-    int num;
+    String[] name = new String[10];
+    int[][] data = new int[10][5];
+    String str1 = "";
+    String str2 = "";
+
+    public abstract void showInformation();
 }
 
 class Student extends Parent {
-    private int num;
+    private int num = 0;
 
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-    void ShowInformation() {
+    @Override
+    public void showInformation() {
         try {
-            System.out.print("請輸入有幾位學生？：");
-            num = sc.nextInt();
+            out.print("請輸入學生人數：");
+            num = scanner.nextInt();
+
             // 陣列索引值必須從0開始
-            // for(int j=1;j<=num;j++) {
             for (int j = 0; j < num; j++) {
-                System.out.println("請輸入第" + j + "學生的名字");
-                super.name[j] = sc.next();
-                System.out.println("請輸入第" + j + "學生的年齡:");
-                super.data[j][0] = sc.nextInt();
-                System.out.println("請輸入第" + j + "學生的學號:");
-                super.data[j][1] = sc.nextInt();
-                System.out.println("請輸入第" + j + "學生的年級:");
-                super.data[j][2] = sc.nextInt();
-                System.out.println("請輸入第" + j + "學生的總平均:");
-                super.data[j][3] = sc.nextInt();
+                out.printf("請輸入第 %d 學生的名字：", (j + 1));
+                super.name[j] = scanner.next();
+                out.printf("請輸入第 %d 學生的年齡：", (j + 1));
+                super.data[j][0] = scanner.nextInt();
+                out.printf("請輸入第 %d 學生的學號：", (j + 1));
+                super.data[j][1] = scanner.nextInt();
+                out.printf("請輸入第 %d 學生的年級：", (j + 1));
+                super.data[j][2] = scanner.nextInt();
+                out.printf("請輸入第 %d 學生的總平均：", (j + 1));
+                super.data[j][3] = scanner.nextInt();
             }
 
         } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-            System.out.println("你的值超出陣列範圍！！");
-            System.out.println("錯誤訊息" + e);
+            out.println("你的索引值超出陣列範圍！");
+            out.println("錯誤訊息：" + e);
         } finally {
-            System.out.println("謝謝使用 再見！！");
+            out.println("謝謝使用，再見。");
         }
     }
 }
 
 class Teacher extends Parent {
-    private int num;
-    Scanner sc = new Scanner(System.in);
+    private int num = 0;
+    Scanner scanner = new Scanner(System.in);
 
-    void ShowInformation() {
-        System.out.print("請輸入有幾位老師？：");
-        num = sc.nextInt();
-        // for(int j=1;j<=num;j++) {
+    @Override
+    public void showInformation() {
+        System.out.print("請輸入有老師人數：");
+        num = scanner.nextInt();
         for (int j = 0; j < num; j++) {
             try {
-                System.out.println("請輸入第" + j + "老師的名字:");
-                super.name[j] = sc.next();
-                System.out.println("請輸入第" + j + "老師的年齡:");
-                super.data[j][0] = sc.nextInt();
-                System.out.println("請輸入第" + j + "老師的學號:");
-                super.data[j][1] = sc.nextInt();
-                System.out.println("請輸入第" + j + "老師的年級:");
-                super.data[j][2] = sc.nextInt();
-                System.out.println("請輸入第" + j + "老師的總平均:");
-                super.data[j][3] = sc.nextInt();
+                out.printf("請輸入第 %d 老師的名字：", (j + 1));
+                super.name[j] = scanner.next();
+                out.printf("請輸入第 %d 老師的年齡：", (j + 1));
+                super.data[j][0] = scanner.nextInt();
+                out.printf("請輸入第 %d 老師的學號：", (j + 1));
+                super.data[j][1] = scanner.nextInt();
+                out.printf("請輸入第 %d 老師的年級：", (j + 1));
+                super.data[j][2] = scanner.nextInt();
+                out.printf("請輸入第 %d 老師的總平均：", (j + 1));
+                super.data[j][3] = scanner.nextInt();
 
             } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-                System.out.println("你的值超出陣列範圍！！");
-                System.out.println("錯誤訊息" + e);
+                out.println("你的值超出陣列範圍！！");
+                out.println("錯誤訊息：" + e);
             } finally {
-                System.out.println("謝謝使用 再見！！");
+                out.println("謝謝使用，再見。");
             }
         }
 
@@ -79,10 +84,10 @@ public class Ch11 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Student s = new Student();
-        Teacher t = new Teacher();
-        s.ShowInformation();
-        t.ShowInformation();
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+        student.showInformation();
+        teacher.showInformation();
     }
 
 }
@@ -95,3 +100,5 @@ public class Ch11 {
 //請問有什麼處理方法嘛？
 //code:https://ideone.com/fork/vwMeqq
 //https://www.facebook.com/groups/1403852566495675/permalink/2557379531142967/
+
+//https://ideone.com/fork/sIReHL
