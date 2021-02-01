b5e5830744389b8e31ea4c092edb103a22cb19c0
KuoHsiangYu2 <bbb105013@nptu.edu.tw>
2021-02-01 22:59:59 +0800

    1. 把註解寫得更詳細。 2. 控制 [列數] 的變數重新命名成 row ，控制 [行數] 的變數重新命名成 column 。

--- Java程式_練習/二維陣列練習/TestArray1.java
+++ Java程式_練習/二維陣列練習/TestArray1.java
@@ -12,23 +12,23 @@ public class TestArray1 {
         };
 
         System.out.println("先執行數列加總的動作-start");
-        for (int i = 0; i <= 4; i++) {
-            for (int j = 1; j <= 5; j++) {
+        for (int row = 0; row <= 4; row++) {
+            for (int column = 1; column <= 5; column++) {
                 /* 先執行數列加總的動作。 */
                 /* 第1行 = [50 + 30 + 70 + 66 + 22] */
-                System.out.printf("array[5][%d] = array[5][%d] + array[%d][%d];%n", j, j, i, j);
-                array[5][j] = array[5][j] + array[i][j];
+                System.out.printf("array[5][%d] = array[5][%d] + array[%d][%d];%n", column, column, row, column);
+                array[5][column] = array[5][column] + array[row][column];
             }
         }
         System.out.println("先執行數列加總的動作-end");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int column = 1; column <= 5; column++) {
             /* 再把加總的數字除以5 */
-            array[5][i] = array[5][i] / 5;
+            array[5][column] = array[5][column] / 5;
         }
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.printf("%02d, ", array[i][j]);
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                System.out.printf("%02d, ", array[row][column]);
             }
             System.out.println();
         }
@@ -60,4 +60,47 @@ Sample Output:
 加總列數為 第 0 列 ~ 第 4 列。
 array[5][1] = (array[0][1] + array[1][1] + array[2][1] + array[3][1] + array[4][1]) ÷ 5
 其餘依此類推。
+
+程式執行結果：
+先執行數列加總的動作-start
+array[5][1] = array[5][1] + array[0][1];
+array[5][2] = array[5][2] + array[0][2];
+array[5][3] = array[5][3] + array[0][3];
+array[5][4] = array[5][4] + array[0][4];
+array[5][5] = array[5][5] + array[0][5];
+array[5][1] = array[5][1] + array[1][1];
+array[5][2] = array[5][2] + array[1][2];
+array[5][3] = array[5][3] + array[1][3];
+array[5][4] = array[5][4] + array[1][4];
+array[5][5] = array[5][5] + array[1][5];
+array[5][1] = array[5][1] + array[2][1];
+array[5][2] = array[5][2] + array[2][2];
+array[5][3] = array[5][3] + array[2][3];
+array[5][4] = array[5][4] + array[2][4];
+array[5][5] = array[5][5] + array[2][5];
+array[5][1] = array[5][1] + array[3][1];
+array[5][2] = array[5][2] + array[3][2];
+array[5][3] = array[5][3] + array[3][3];
+array[5][4] = array[5][4] + array[3][4];
+array[5][5] = array[5][5] + array[3][5];
+array[5][1] = array[5][1] + array[4][1];
+array[5][2] = array[5][2] + array[4][2];
+array[5][3] = array[5][3] + array[4][3];
+array[5][4] = array[5][4] + array[4][4];
+array[5][5] = array[5][5] + array[4][5];
+先執行數列加總的動作-end
+01, 50, 60, 70, 00, 00, 00, 
+02, 30, 40, 50, 00, 00, 00, 
+03, 70, 80, 90, 00, 00, 00, 
+04, 66, 77, 88, 00, 00, 00, 
+05, 22, 33, 44, 00, 00, 00, 
+00, 47, 58, 68, 00, 00, 00, 
+
+程式執行環境：
+電腦作業系統：Windows 10 家用版
+CPU：Intel(R) Core(TM) i5-8400 CPU @ 2.80GHz   2.81 GHz
+RAM：12.0 GB
+IDE：Eclipse IDE for Enterprise Java Developers 2020-09 (4.17.0)
+jdk version：1.8.0_162
+jre version：1.8.0_162
 */
